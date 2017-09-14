71cb20730ec12e5c5b15ed3b679693b1bb539a61
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2017-09-14 11:48:31 -0400

    CharacterA added and Updated TheDuel

---
+++ DuelProjectRemington/src/CharacterA.java
@@ -0,0 +1,14 @@
+public class CharacterA implements Dueler{
+	private int hp;
+	public CharacterA() {
+		
+	}
+	public String.getName() {
+		return "Dirty Dan";
+	}
+	public void taunt() {
+		System.out.println("That was SUCH a good move!");
+	}
+}
+
+
